a039a8b3da28d69ec0e7bbe372b8e6a82c48177e
Andrei Vakar <Andrei.Vakar@itechart-group.com>
2020-03-06 12:05:06 +0300

    Updating Akka version

--- AkkaTask/src/main/scala/by/itechart/supervisor/actor/CompanyActor.scala
+++ AkkaTask/src/main/scala/by/itechart/supervisor/actor/CompanyActor.scala
@@ -1,38 +1,13 @@
 package by.itechart.supervisor.actor
 
-import akka.actor.{ActorLogging, ActorRef, Props}
-import akka.persistence.{PersistentActor, SnapshotOffer, SnapshotSelectionCriteria}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import by.itechart.action._
 
-sealed trait Evt
-case class UpdatedUserMessage() extends Evt
-
-case class ExampleState(count: Int = 0) {
-  def increment(): ExampleState = copy(count + 1)
-
-  def clean(): ExampleState = ExampleState(0)
-}
-
-class CompanyActor(name: String) extends PersistentActor with ActorLogging {
+class CompanyActor(name: String) extends Actor with ActorLogging {
   private var userNameToActor = Map.empty[String, ActorRef]
+  private var count = 0
 
-  override def persistenceId = name
-
-  var state = ExampleState()
-
-  def incrementState(): Unit =
-    state = state.increment()
-
-  def cleanState(): Unit =
-    state = state.clean()
-
-  val receiveRecover: Receive = {
-    case evt: Evt => incrementState()
-    case SnapshotOffer(_, snapshot: ExampleState) => state = snapshot
-  }
-
-  val snapShotInterval = 1000
-  val receiveCommand: Receive = {
+  def receive = {
     case message: CreateUser => {
       val ref = context.actorOf(Props[UserActor], name = message.userName)
       userNameToActor += message.userName -> ref
@@ -40,16 +15,10 @@ class CompanyActor(name: String) extends PersistentActor with ActorLogging {
     case message: UpdateUserMessage => {
       val userActor = userNameToActor(message.messageToUser.userName)
       userActor ! message
-      persist(UpdatedUserMessage()) { event =>
-        incrementState()
-        context.system.eventStream.publish(event)
-        /*if (lastSequenceNr % snapShotInterval == 0 && lastSequenceNr != 0)
-          saveSnapshot(state)*/
-        deleteSnapshots(SnapshotSelectionCriteria())
-      }
+      count += 1
     }
     case message: UpdateCompanyMessage => {
-      message.newspaperActor ! PrintAmountCompanyMessages(message.messageToCompany.companyName, state.count)
+      message.newspaperActor ! PrintAmountCompanyMessages(message.messageToCompany.companyName, count)
     }
   }
 }
